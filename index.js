7641798725f595521ba3ce33a38496e287e54355
Programming Hero <programming.hero1@gmail.com>
2025-10-30 21:24:24 -0500

    added environment variable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,8 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json())
 
-
-const uri = "mongodb+srv://smartdbUser:JOFV2xVJ1Ri6UmrT@cluster0.vyznij5.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vyznij5.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
     serverApi: {
